4b63858fbc54b2a0c46894690f7e853f2b16bca5
Nishanth Shanmugham <nishanths@utexas.edu>
2016-04-25 23:11:54 -0500

    Add spark demos

---
+++ spark/scp-jar.sh
@@ -0,0 +1,4 @@
+scp -i ~/.ssh/aws-ec2-spark.pem "$1" root@ec2-54-86-243-145.compute-1.amazonaws.com:~/
+scp -i ~/.ssh/aws-ec2-spark.pem "$1" root@ec2-52-87-243-140.compute-1.amazonaws.com:~/
+scp -i ~/.ssh/aws-ec2-spark.pem "$1" root@ec2-52-90-87-181.compute-1.amazonaws.com:~/
+scp -i ~/.ssh/aws-ec2-spark.pem "$1" root@ec2-52-201-216-171.compute-1.amazonaws.com:~/
